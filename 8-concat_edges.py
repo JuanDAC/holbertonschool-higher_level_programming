b59f718c6a12d7e2e28cf3c63a62eb41f2447877
JuanDAC <juanavila12.0308@gmail.com>
2021-04-26 21:17:22 +0000

    Python hello word

---
+++ 8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("{}{}{}".format(str[39:67], str[107:112], str[0:6]));
