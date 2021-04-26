b70645239d931e0b67d21115cf82275ef2e00a9a
JuanDAC <juanavila12.0308@gmail.com>
2021-04-26 23:32:44 +0000

    (fix) [Create a new sentence]

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("{}{}{}".format(str[39:67], str[107:112], str[0:6]))
+print("%s%s%s" % (str[39:67], str[107:112], str[0:6]))
+
