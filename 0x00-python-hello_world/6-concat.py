bfcc0003dba95fb8c9ee4ff637454bf04a7caa51
JuanDAC <juanavila12.0308@gmail.com>
2021-04-26 22:54:34 +0000

    (fix) [Play with strings]

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("Welcome to {0} {1}!".format(str1, str2))
+str1 = "%s %s" % (str1, str2);
+print("Welcome to {}!".format(str1))
