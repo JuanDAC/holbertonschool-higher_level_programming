8f17389c5daa5af01a900b0ec55ea65952927774
JuanDAC <juanavila12.0308@gmail.com>
2021-04-27 22:29:45 +0000

    [0x01 ~ 5] (path 0)

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{:d}".format(number), end='\n' if number == 99 else ", ")
+    print("{:02d}".format(number), end='\n' if number == 99 else ", ")
