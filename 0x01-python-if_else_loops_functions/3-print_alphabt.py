6e4ed7ac3c7039be8fc0e3285b5bed1cb6416e1f
JuanDAC <juanavila12.0308@gmail.com>
2021-04-27 21:58:57 +0000

    [0x01 ~ 4] (create)

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
-        print("{:c}".format(character), end='')
+for number in range(0, 98):
+   print("{:d} = 0x{:x}".format(number, number))
