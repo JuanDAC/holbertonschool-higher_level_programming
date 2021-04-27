d9dbe44f0f79a8b043f82241456647e830c90096
JuanDAC <juanavila12.0308@gmail.com>
2021-04-27 00:06:10 +0000

    (fix) [Create a new sentence checker]

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "%s%s%s" % (str[39:66], str[106:112], str[0:6])
+str = "%s%s%s" % (str[39:67], str[107:112], str[:6])
 print(str)
