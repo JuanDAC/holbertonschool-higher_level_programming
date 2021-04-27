c47d5234611db2e28a6a8a1ff4aca589d695adf1
JuanDAC <juanavila12.0308@gmail.com>
2021-04-27 21:21:45 +0000

    [0x01 ~ 2] (refactor - order if)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,9 +9,9 @@ else:
 
 message = "Last digit %d of %d and is" % (number, last_digit)
 
-if last_digit > 5:
-    print(message, "greater than 5")
-elif last_digit == 0:
+if last_digit == 0:
     print(message, "0")
+elif last_digit > 5:
+    print(message, "greater than 5")
 else:
     print(message, "less than 6 and not 0")
