51091082bad667706715aa7d9312779fed9b0c62
JuanDAC <juanavila12.0308@gmail.com>
2021-04-28 03:22:36 +0000

    [0x01 ~ 8](FIX - uppercas)

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-uper = lambda x: x i  
-def uppercase(str):
-    print("{:s}".format("".join(str.split()))
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        if ord(character) >= 97 and ord(character) <= 122:
+            string_new += "{:c}".format(ord(character) - 32)
+        else:
+            string_new += "{:s}".format(character)
+    print("{:s}".format(string_new))
+
