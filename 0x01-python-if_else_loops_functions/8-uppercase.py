6cc19f2774df0de06ce56736c86834a5b73c4fb3
JuanDAC <juanavila12.0308@gmail.com>
2021-04-28 03:42:30 +0000

    [0x01 ~ 8](REFACTO: extras to function)

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
 def uppercase(string):
     string_new = ""
     for character in string:
-        if ord(character) >= 97 and ord(character) <= 122:
-            string_new += "{:c}".format(ord(character) - 32)
-        else:
-            string_new += "{:s}".format(character)
+        string_new += "%c" % to_uper(character);
     print("{:s}".format(string_new))
+
