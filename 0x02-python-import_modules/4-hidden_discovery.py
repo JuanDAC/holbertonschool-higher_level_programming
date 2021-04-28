af6eb781d55fee8619e89d27be864fb1e3512578
JuanDAC <juanavila12.0308@gmail.com>
2021-04-28 22:04:49 +0000

    [0x02 ~ 4] (create)

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for attribute in dir(hidden_4):
-        if attribute[0]  != '_':
+        if attribute[0] != '_' and attribute[1] != '_':
             print(attribute)
