431d54a5620a34602d80104fc2c176e1dd39bad4
JuanDAC <juanavila12.0308@gmail.com>
2021-05-05 22:24:28 +0000

    [0x03 ~ 6-print_matrix_integer.py] (create)

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/bin/
+
+
+def print_matrix_integer(matrix=[[]]):
+    height = len(matrix)
+    init = 0
+
+    for i in range(init, height):
+        width = len(matrix[i]);
+        if not width:
+            print("")
+        for j in range(init, width):
+            print(
+                "{:d}".format(matrix[i][j]),
+                end=('\n' if width == j + 1 else ' ')
+            )
