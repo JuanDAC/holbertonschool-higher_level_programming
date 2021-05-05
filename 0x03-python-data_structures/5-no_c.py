c2669b8eeaff2004de0178f3039056512be6f534
JuanDAC <juanavila12.0308@gmail.com>
2021-05-05 21:23:15 +0000

    [0x03 ~ 5-no_c.py] (alternative)

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,12 +2,7 @@
 
 
 def no_c(my_string):
-
-    new_string = ""
-    length = len(my_string)
-    init = 0
-
-    for i in range(init, length):
-        if my_string[i] != 'c' or my_string[i] != 'C':
-            new_string += my_string[i]
+    new_string = my_string.translate(
+        {ord(i): None for i in "Cc"}
+    )
     return new_string
