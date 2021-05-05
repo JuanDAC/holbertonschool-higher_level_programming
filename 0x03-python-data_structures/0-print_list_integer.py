5fd4192cf78785cade34e77e8e4723e8b9660709
JuanDAC <juanavila12.0308@gmail.com>
2021-05-05 18:35:12 +0000

    [0x03 ~ 0-print_list_integer.py] (add cast)

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,6 +5,6 @@ def print_list_integer(my_list=[]):
     length = len(my_list)
     for i in range(length):
         print(
-            "{}".format(my_list[i]),
+                "{:d}".format(my_list[i]),
             end=('\n' if length != (i + 1) else '')
         )
