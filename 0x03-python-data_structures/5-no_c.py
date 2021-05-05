5563fd3e1711e3f612cd7badba9d9b377d08d49c
JuanDAC <juanavila12.0308@gmail.com>
2021-05-05 20:49:26 +0000

    [0x03 ~ 5-no_c.py] (fix: add return original list)

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    if not my_string:
+        return ""
+
+    new_string = ""
+    length = len(my_string)
+    init = 0
+
+    for i in range(init, length):
+        if my_string[i] != 'c' or my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string
