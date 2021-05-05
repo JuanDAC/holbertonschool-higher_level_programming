9a09a46daed687c6381a0fd0344126c784cb8ada
JuanDAC <juanavila12.0308@gmail.com>
2021-05-05 20:11:30 +0000

    [0x03 ~ 3-print_reversed_list_integer.py] (hadler list error)

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,11 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     length = len(my_list)
+    init = 1
 
-    for i in range(1, length + 1):
+    for i in range(init, length + 1):
         print("{:d}".format(my_list[i * -1]))
+
