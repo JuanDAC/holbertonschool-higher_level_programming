3ced571c72c883729d617e374a38fb7bfa447dbe
JuanDAC <juanavila12.0308@gmail.com>
2021-05-06 21:26:17 +0000

    [0x04 - 6-print_sorted_dictionary.py] (fix: delete return)

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
 
-def my_format(a_dictionary):
-    def warp(key):
-        print("{:s}:".format(key), a_dictionary[key])
-    return warp
-
-
 def print_sorted_dictionary(a_dictionary):
     if not a_dictionary:
         return
-    map(my_format(a_dictionary), sorted(a_dictionary))
+    for key in sorted(a_dictionary):
+        print("{:s}:".format(key), a_dictionary[key])
