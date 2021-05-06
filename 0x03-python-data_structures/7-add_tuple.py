4eb68eb882a33d9d4788ecb95165082869f04608
JuanDAC <juanavila12.0308@gmail.com>
2021-05-06 00:10:48 +0000

    [0x03 ~ 7-add_tuple.py] (style)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     len_max = 2
     array_a = [0 for i in range(len_max)]
     array_b = [0 for i in range(len_max)]
     for i in range(len_max):
         if (i < len(tuple_a)):
-            array_a[i] = tuple_a[i];
+            array_a[i] = tuple_a[i]
         if (i < len(tuple_b)):
-            array_b[i] = tuple_b[i];
+            array_b[i] = tuple_b[i]
 
     return (array_a[0] + array_b[0], array_a[1] + array_b[1])
