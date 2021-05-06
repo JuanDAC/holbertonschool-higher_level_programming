88e335d52662ce452694183184c8ae9d9ce42954
JuanDAC <juanavila12.0308@gmail.com>
2021-05-06 23:00:16 +0000

    [0x04 - 10] (fix: python version)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,18 +2,6 @@
 
 
 def best_score(a_dictionary):
-    value = 1
-    key = 0
-    my_list = []
-    currentItem = (None,)
-
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-
-    my_list = a_dictionary.items()
-    currentItem = my_list[0] if len(my_list) else (None, None)
-
-    for i in range(len(my_list)):
-        if currentItem[value] < my_list[i][value]:
-            currentItem = my_list[i]
-    return currentItem[key]
+    return max(a_dictionary, key=a_dictionary.get)
