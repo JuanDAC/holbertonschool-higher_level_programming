331633e4469bd7018cafbab4f8908684d1a027f8
JuanDAC <juanavila12.0308@gmail.com>
2021-05-06 00:32:40 +0000

    [0x03 ~ 8-multiple_returns.py] (fix len)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@
 def multiple_returns(sentence):
     return (
         len(sentence),
-        sentence[first] if len(sentence) else None
+        sentence[first] if len(sentence) == 0 else None
     )
