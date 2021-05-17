e554c617a0d1c6c41436b764233c4f1cd6a3e8c9
JuanDAC <juanavila12.0308@gmail.com>
2021-05-17 20:07:11 +0000

    0x05 ~ fix double error handler 2-safe_print_list_integers.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,16 +2,14 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    number_characters = 0
     string = ""
     for i in range(x):
         try:
             string += "{}".format(my_list[i])
         except IndexError:
-            i -= 1
             break
+        else:
+            number_characters += 1
     print(string)
-    try:
-        i += 1
-    except UnboundLocalError:
-        i = 0
-    return i
+    return number_characters
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,13 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
+    number_characters = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            (string)
-        except ValueError:
-            i -= 1
+        except (ValueError, TypeError):
             continue
-        except TypeError:
-            i -= 1
-            continue
+        else:
+            number_characters += 1
     print("")
-    return i
+    return number_characters
