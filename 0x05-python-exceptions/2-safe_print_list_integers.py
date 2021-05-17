7bbc5f5b09c93ebddbd84686e329bda7323a807a
JuanDAC <juanavila12.0308@gmail.com>
2021-05-17 19:09:46 +0000

    0x05 ~ fix handler TypeError 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,15 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    string = ""
     for i in range(x):
         try:
-            string += "{:d}".format(my_list[i])
+            print("{:d}".format(my_list[i]), end="")
+            (string)
         except ValueError:
             i -= 1
             continue
         except TypeError:
             i -= 1
             continue
-    print(string)
+    print("")
     return i
