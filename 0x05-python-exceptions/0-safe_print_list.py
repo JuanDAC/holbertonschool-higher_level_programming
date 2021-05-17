6a01f01c6cde6ddb3ab9a9826ec056c4f27d46ff
JuanDAC <juanavila12.0308@gmail.com>
2021-05-17 17:46:07 +0000

    create readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    string = ""
+    for i in range(x):
+        try:
+            string += "{}".format(my_list[i])
+        except IndexError:
+            i -= 1
+            break
+    print(string)
+    try:
+        i += 1
+    except UnboundLocalError:
+        i = 0;
+    return i;
