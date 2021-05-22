d19a2491a5e1495abd39073742f01b525d2a31a7
JuanDAC <juanavila12.0308@gmail.com>
2021-05-22 13:36:17 -0500

    0x06 ~ style: space blank arownd equals sign

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size = 0):
         """__init__ constructor method."""
-        if type(size) is int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
