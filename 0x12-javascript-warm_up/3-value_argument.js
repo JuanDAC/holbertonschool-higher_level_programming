9baa695e003764e4ed96c7cdf59f1933ab62bd44
JuanDAC <juanavila12.0308@gmail.com>
2021-08-02 15:05:32 -0400

    [fix] (validate valid value in index argv)

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const argv = require('process').argv;
-
-console.log((argv.length > 2) ? argv[2] : 'No argument');
+const { argv } = require('process');
+console.log(argv[2] ? `${argv[2]}` : 'No argument');
