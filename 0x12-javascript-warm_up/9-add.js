373bb1d0ef0cd3364c3dd7595310dd4822fca5f8
JuanDAC <juanavila12.0308@gmail.com>
2021-08-02 20:09:27 -0400

    [feature] (add to export)

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const argv = require('process').argv;
-
-const operator = parseInt(Number(argv[3]));
-const operating = parseInt(Number(argv[2]));
-
-if (isNaN(operator) || isNaN(operating)) {
-  console.log('NaN');
-} else {
-  console.log(operating + operator);
-}
+const add = (a, b) => a + b;
+module.add = add;
