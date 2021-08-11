5c5b27d657d85892563ceecffee8f42f5b042269
JuanDAC <juanavila12.0308@gmail.com>
2021-08-11 15:54:09 -0400

    [fix] (print in 9 and zero values in list in 8)

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,5 +4,8 @@ exports.esrever = function (list) {
   if (list.length === 1) {
     return ([list.pop()]);
   }
+  if (list.length === 0) {
+    return ([]);
+  }
   return [list.pop()].concat(exports.esrever(list));
 };
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 exports.logMe = ((counter = 0) => function (item) {
-  console.log(`${counter++} ${item}`);
+  console.log(`${counter++}: ${item}`);
 })();
