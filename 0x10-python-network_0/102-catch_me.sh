1ed304b2d76ea734adb505ad365d4ff811f90b66
JuanDAC <juanavila12.0308@gmail.com>
2021-08-20 00:17:53 -0400

    [feature] (addvanced)

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
