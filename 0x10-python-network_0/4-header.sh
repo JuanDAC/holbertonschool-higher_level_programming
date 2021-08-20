e3a040b0ab8739edec5b6dd0ef92cc1c931e251d
JuanDAC <juanavila12.0308@gmail.com>
2021-08-19 23:08:03 -0400

    [fix] (feature)

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that takes in a URL as an argument
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a POST request to the passed URL
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
