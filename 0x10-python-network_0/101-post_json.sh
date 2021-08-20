93c417163fa37509dff2a6ce1ca901037c1c455b
JuanDAC <juanavila12.0308@gmail.com>
2021-08-20 00:23:01 -0400

    [fix] (without method POST)

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Script that sends a POST request with some information
-curl -sH "Content-Type: application/json" -X POST -d @"$2" "$1"
+# Script that sends a POST request with any information
+curl -sH "Content-Type: application/json" -d @"$2" "$1"
