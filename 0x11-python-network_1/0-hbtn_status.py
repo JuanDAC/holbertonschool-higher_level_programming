46ba0a9953de0d8264f5f1ac0c89a84b02cda3c3
JuanDAC <juanavila12.0308@gmail.com>
2021-08-21 00:55:19 -0400

    [style] (whitespace before ':')

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@
 from urllib.request import urlopen
 
 actions = [
-        ("type", lambda html : type(html)),
-        ("content", lambda html : html),
-        ("utf8 content", lambda html : html.decode()),
+        ("type", lambda html: type(html)),
+        ("content", lambda html: html),
+        ("utf8 content", lambda html: html.decode()),
 ]
 
 with urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    [print("\t- {}: {}".format(header, value(html))) for header, value in actions]
+    [print("\t- {}: {}".format(header, fun(html))) for header, fun in actions]
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python script that takes in a URL and prints its header X-Request-Id
+# Python script that takes in a URL and prints header X-Request-Id
 
 from urllib.request import urlopen
 from sys import argv
@@ -7,5 +7,5 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     with urlopen(url) as response:
-        headers_like_dict = dict(response.getheaders());
+        headers_like_dict = dict(response.getheaders())
         print(headers_like_dict['X-Request-Id'])
