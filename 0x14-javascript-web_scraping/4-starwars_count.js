f1d87fc513a0fe4ffa4b0423c191524b9c6be22e
JuanDAC <juanavila12.0308@gmail.com>
2021-09-14 20:41:13 -0400

    [feature] (Star wars Wedge Antilles)

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const { get } = require('request');
+const { argv } = require('process');
+const url = argv[2];
+
+get(url, (err, res, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const { results } = JSON.parse(data);
+  const count = [0, ...results]
+    .reduce((prevr, { characters }) => prevr + [0, ...characters]
+      .reduce((prevc, character) => prevc + character.includes('18')));
+  console.log(count);
+});
